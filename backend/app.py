3ff6a1976fd214d0361ac68a6838effb19a74bc7
Muhammad Quraisy <iye.fredickson@gmail.com>
2025-02-16 09:33:40 +0700

    fix: remove redundant print statement and adjust logging for static folder

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,5 @@ from src import app
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), 'static/images/drawings')
 
-print(app.config['UPLOAD_FOLDER'])
-
 if __name__ == '__main__':
     app.run(debug=True)
